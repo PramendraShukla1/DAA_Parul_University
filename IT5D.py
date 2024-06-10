5de788639a37dac79e6d5ab1c7e1475f6ceae214
Pramendra Shukla <shuklapramendra99@gmail.com>
2024-06-10 10:37:14 +0530

    10/06/24

--- IT5D.py
+++ IT5D.py
@@ -1,3 +1,29 @@
-a = 12
-b = 24
-print(a+b)
+
+'''
+
+def addTwo(a,b):
+    return a+b
+
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+print(addTwo(a,b))
+
+
+
+def maximum(a, b, c):
+    if (a>=b) and (a>=c):
+        largest = a
+    elif(b>=a) and (b>=c):
+            largest = b
+    else:
+        largest = c
+        return largest
+
+
+a = int(input(" Enter number one "))
+b = int(input(" Enter number two "))
+c = int(input(" Enter number three"))
+
+print(maximum(a,b,c))
+
+'''
