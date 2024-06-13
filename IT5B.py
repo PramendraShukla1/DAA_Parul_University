8a0277407f79f479ec79be0d76b58a0c6249ebd1
Pramendra Shukla <shuklapramendra99@gmail.com>
2024-06-13 23:01:40 +0530

    Leet code file updated

---
+++ IT5B.py
@@ -0,0 +1,20 @@
+# Write a program to check if a given number is prime or not
+'''
+
+
+def checkPrime(num):
+    for i in range (2, num):
+        if num % i ==0:
+            return False
+    return True
+
+num = int(input("Enter a number -> "))
+result = checkPrime(num)
+
+if result == True:
+    print("The number is prime")
+else:
+    print("The number is not prime")
+
+'''
+
---
+++ LeetCode.py
@@ -0,0 +1,154 @@
+
+'''
+1051. Height Checker
+Link: https://leetcode.com/problems/height-checker/description/?envType=daily-question&envId=2024-06-10
+
+
+def heightChecker(heights):
+    copyHeights = sorted(heights)
+    count = 0
+    for i in range (len(heights)):
+        if heights[i] != copyHeights[i]:
+            count+=1
+    return count
+
+a = [1,1,4,2,1,3]
+print(heightChecker(a))
+'''
+#--------------------------------------------------#
+'''
+26. Remove Duplicates from Sorted Array
+Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+def removeDuplicates(nums):
+    left = 1
+
+    for right in range(1,len(nums)):
+        if nums[right] != nums[right - 1]:
+            nums[left] = nums[right]
+            left+=1
+    return left
+
+
+a = [1,1,2]
+a = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(a))
+
+'''
+#--------------------------------------------------#
+'''
+1122. Relative Sort Array
+link: https://leetcode.com/problems/rel-sort-array/
+
+def relativeSortArray(arr1, arr2):
+    res = []
+    for a in arr2:
+        for i, v in enumerate(arr1):
+            if a == v:
+                res.append(arr1[i])
+    res = res+sorted([a for a in arr1 if a not in arr2])
+    return res
+
+
+arr1 = [2,3,1,3,2,4,6,7,9,2,19]
+arr2 = [2,1,4,3,9,6]
+print(relativeSortArray(arr1,arr2))
+
+'''
+#--------------------------------------------------#
+'''
+75. Sort Colors
+Link: https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2024-06-12
+
+def sortColors(nums):
+    n = len(nums)
+
+    for i in range(n-1):
+        swapped = False
+        for j in range(0, n-i-1):
+            if nums[j]>nums[j+1]:
+                swapped = True
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+
+        if not swapped:
+            return
+
+
+
+num = [2,0,2,1,1,0]
+sortColors(num)
+for i in range(len(num)):
+    print(num[i])
+    
+'''
+#--------------------------------------------------#
+'''
+344. Reverse String
+Link: https://leetcode.com/problems/reverse-string/description/?envType=daily-question&envId=2024-06-02
+
+def reverseString(s):
+    left, right = 0, len(s)-1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left +=1
+        right -= 1
+
+
+s = ["h","e","l","l","o"]
+reverseString(s)
+print(s)
+'''
+#--------------------------------------------------#
+'''
+1979. Find Greatest Common Divisor of Array
+Link: https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+
+def findGCD(nums):
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+    min_num = min(nums)
+    max_num = max(nums)
+
+    return gcd(min_num, max_num)
+
+
+nums = [2, 5, 6, 9, 10]
+print(findGCD(nums))
+
+'''
+#--------------------------------------------------#
+'''
+344. Reverse String
+Link: https://leetcode.com/problems/reverse-string/
+
+def reverseString(s):
+    left, right = 0, len(s)-1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left+=1
+        right-=1
+
+s = ["h","e","l","l","o"]
+reverseString(s)
+print(s)
+'''
+#--------------------------------------------------#
+'''
+2037. Minimum Number of Moves to Seat Everyone
+Link: https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+
+def minMoves(seats, students):
+    temp = 0
+    seats.sort()
+    students.sort()
+    for i in range(len(seats)):
+        temp += abs(seats[i] - students[i])
+    return temp
+
+seats = [3,1,5]
+students = [2,7,4]
+print(minMoves(seats, students))
+'''
+#--------------------------------------------------#
